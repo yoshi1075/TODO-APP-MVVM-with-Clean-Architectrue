63c4a90e6f49893f81d1f217653181deb835b684
yoshi1075 <agy6786@gmail.com>
2024-02-18 12:44:25 +0900

    * 複数のTodoのタイトルを表示

--- app/src/main/java/com/example/todo_app_mvvm_with_clean_architectrue/MainActivity.kt
+++ app/src/main/java/com/example/todo_app_mvvm_with_clean_architectrue/MainActivity.kt
@@ -11,22 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todo_app_mvvm_with_clean_architectrue.ui.theme.TODOAPPMVVMwithCleanArchitectrueTheme
+import com.example.todo_app_mvvm_with_clean_architectrue.ui.todo_list.TodoListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TODOAPPMVVMwithCleanArchitectrueTheme {
-
+                TodoListScreen()
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TODOAPPMVVMwithCleanArchitectrueTheme {
-
     }
 }
