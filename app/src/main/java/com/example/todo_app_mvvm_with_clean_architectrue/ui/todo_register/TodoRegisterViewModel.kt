c6344ce0ff8377d664ec62e95724ceffa795001c
yoshi1075 <agy6786@gmail.com>
2024-02-23 14:54:14 +0900

    Todo登録画面のViewModelを作成 #20

---
+++ app/src/main/java/com/example/todo_app_mvvm_with_clean_architectrue/ui/todo_register/TodoRegisterViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.todo_app_mvvm_with_clean_architectrue.ui.todo_register
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todo_app_mvvm_with_clean_architectrue.data.Todo
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class TodoRegisterViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(TodoRegisterUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun onTitleUpdated(title: String) {
+        _uiState.update {
+            uiState.value.copy(title = title)
+        }
+    }
+
+    fun onDetailUpdated(detail: String) {
+        _uiState.update {
+            uiState.value.copy(detail = detail)
+        }
+    }
+
+    fun onRegisterButtonTapped() {
+        viewModelScope.launch {
+            val todo = Todo(
+                title = uiState.value.title,
+                detail = uiState.value.detail
+            )
+            registerTodo(todo)
+                .onEach { isSuccess ->
+                    // TODO: display result
+                    Log.d("register isSuccess: ", isSuccess.toString())
+                }
+                .launchIn(this)
+        }
+    }
+
+    private suspend fun registerTodo(todo: Todo): Flow<Boolean> = flow {
+        // TODO: DBにTodoを登録
+        delay(500L)
+        emit(true)
+    }
+}
