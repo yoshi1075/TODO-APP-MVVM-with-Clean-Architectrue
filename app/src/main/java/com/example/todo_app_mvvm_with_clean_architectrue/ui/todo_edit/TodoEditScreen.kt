4ec3b1c0486cc7919ff18444cc6c47db80ad8625
yoshi1075 <agy6786@gmail.com>
2024-02-29 13:14:13 +0900

    Todo登録/編集画面のComposablesをリファクタリング #38

--- app/src/main/java/com/example/todo_app_mvvm_with_clean_architectrue/ui/todo_edit/TodoEditScreen.kt
+++ app/src/main/java/com/example/todo_app_mvvm_with_clean_architectrue/ui/todo_edit/TodoEditScreen.kt
@@ -8,15 +8,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
@@ -25,15 +19,13 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todo_app_mvvm_with_clean_architectrue.ui.todo_edit.components.TodoEditAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -146,49 +138,4 @@ fun TodoEditScreen(
             )
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TodoEditAppBar(
-    backToTodoListScreen: () -> Unit,
-    onEvent: (TodoEditEvent) -> Unit,
-) {
-    TopAppBar(
-        title = { Text("Todo編集", color = Color.White) },
-        navigationIcon = {
-            IconButton(
-                onClick = { backToTodoListScreen() }
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBack,
-                    contentDescription = "Back Arrow",
-                    tint = Color.White,
-                )
-            }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Blue),
-        actions = {
-            IconButton(
-                onClick = { onEvent(TodoEditEvent.OnDeleteIconTapped) }
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Delete,
-                    contentDescription = "Delete",
-                    tint = Color.White,
-                )
-            }
-            IconButton(
-                onClick = {
-                    onEvent(TodoEditEvent.OnCompleteIconTapped)
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Check,
-                    contentDescription = "Check",
-                    tint = Color.White,
-                )
-            }
-        }
-    )
 }
